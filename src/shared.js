f1c072d09881844f2bd383fc820364c1daf245e5
Michelangelo <michael.r.javier@gmail.com>
2026-01-11 16:46:20 -0500

    updated

--- src/shared.js
+++ src/shared.js
@@ -1,3 +1,3 @@
-export const API_URL = process.env.REACT_APP_API_URL || "https://capstone-2-backend-chi.vercel.app";
-export const SOCKETS_URL = process.env.REACT_APP_SOCKETS_URL || "wss://capstone-2-backend-chi.vercel.app";
+export const API_URL = process.env.REACT_APP_API_URL || "https://familytree-backend-6lq7.onrender.com";
+export const SOCKETS_URL = process.env.REACT_APP_SOCKETS_URL || "wss://familytree-backend-6lq7.onrender.com";
 export const NODE_ENV = process.env.NODE_ENV || "development";
