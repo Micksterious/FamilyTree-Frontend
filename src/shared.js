f3c69827ed6818de5a6a581ed2ca5e0dea99a4e5
Michelangelo <michael.r.javier@gmail.com>
2026-01-10 22:21:55 -0500

    updated

--- src/shared.js
+++ src/shared.js
@@ -1,3 +1,3 @@
-export const API_URL = process.env.API_URL || "https://capstone-2-backend-chi.vercel.app";
-export const SOCKETS_URL = process.env.SOCKETS_URL || "wss://capstone-2-backend-chi.vercel.app";
+export const API_URL = process.env.REACT_APP_API_URL || "https://capstone-2-backend-chi.vercel.app";
+export const SOCKETS_URL = process.env.REACT_APP_SOCKETS_URL || "wss://capstone-2-backend-chi.vercel.app";
 export const NODE_ENV = process.env.NODE_ENV || "development";
